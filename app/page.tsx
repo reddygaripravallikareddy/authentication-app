3593e63df9f6a9e02a096101546ab0b9a1bf23e0
Reddygaripravallikareddy <reddygaripravallikareddy@gmail.com>
2026-05-01 14:15:21 +0530

    favicon fixed

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,18 @@
 import HomeComponent from "./component/HomeComponent";
 import { getUserLoggedInStatus } from "./data/users";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
   const router = useRouter();
 
+  useEffect(() => {
+    if (!getUserLoggedInStatus()) {
+      router.push("/login");
+    }
+  }, [router]);
+
   if (!getUserLoggedInStatus()) {
-    router.push("/login");
     return null;
   }
 
